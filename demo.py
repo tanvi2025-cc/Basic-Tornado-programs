510b9214bdb6e5248b0a17c9234be3f0de876439
tanvi2025 <tanvi.ranga@causecode.com>
2018-06-29 15:35:35 +0530

    Request Handler modified

--- demo.py
+++ demo.py
@@ -1,17 +1,34 @@
 #Tornado import statements
 import tornado.ioloop
 import tornado.web
+from tornado.httpclient import HTTPClient
 import os.path
 
 #web server
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('templates/index.html')
+#Request Handlers
+class MyFormHandler(tornado.web.RequestHandler):
+    def get(self):
+       self.render('templates/form.html')
+    def post(self):
+        username = self.get_argument('username')
+        designation = self.get_argument('designation')
+        self.write("Wow " + username + " you're a " + designation)
+  
 
 #Setting the url
 application=tornado.web.Application([
-    (r"/",MainHandler)
+    (r"/",MainHandler),
+    (r"/form",MyFormHandler)
 ])
+def synchronous_fetch(url):
+    http_client = HTTPClient()
+    response = http_client.fetch(url)
+    return response.body
+print(synchronous_fetch('https://google.com'))
+
 #Starting the server
 if __name__=='__main__':
     PortNumber = str(8888)
